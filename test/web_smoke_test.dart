c87c646cdfb7b332379711c2ce8f2cf8f27eb9b7
Berk Han Türkoğlu <tony@Berk-MacBook-Air.local>
2026-05-02 16:17:32 +0300

    test: clean up comments in smoke test

--- test/web_smoke_test.dart
+++ test/web_smoke_test.dart
@@ -1,13 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:serpil_moda_web/main.dart'; // Ensure this path is correct and matches your project structure
+import 'package:serpil_moda_web/main.dart';
 
 void main() {
   testWidgets('Uygulama yükleme testi', (WidgetTester tester) async {
-    // Uygulamayı başlat
-    await tester.pumpWidget(
-        const SerpilModaApp()); // Ensure SerpilModaApp is defined in the imported file
+    await tester.pumpWidget(const SerpilModaApp());
 
-    // Başlıkta 'SERPİL MODA' yazısının olup olmadığını kontrol et
     expect(find.textContaining('SERPİL'), findsWidgets);
   });
 }
